17633cbbb0490cee6276079c7564ac297659e563
SifonFelipe <supermegafelipe@gmail.com>
2024-06-14 18:56:03 -0300

    Driver model added

--- f1prode/base/models.py
+++ f1prode/base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Driver(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    number = models.IntegerField()
+    team_name = models.CharField(max_length=60)
+    country = models.CharField(max_length=70)
+    headshot = models.URLField(max_length=500)
+    #points = models.IntegerField()
+
+    def __str__(self):
+        return self.last_name
