52c46f126d5c384e65e58109e7e401adb830b844
juanmanueldaza <juanmanueldaza@gmail.com>
2024-06-21 15:15:33 -0300

    primera styleada

--- f1prode/base/views.py
+++ f1prode/base/views.py
@@ -56,8 +56,13 @@ def standings(request):
 
 def predict(request):
     drivers = Driver.objects.all()
+    div_numbers = []
+    for driver_number in drivers:
+        number_string = str(driver_number.number)
+        id_complete = "div" + number_string
+        div_numbers.append(id_complete)
 
-    context = {'drivers': drivers}
+    context = {'drivers': drivers, "div_id": div_numbers}
     return render(request, 'base_templates/predicts.html', context)
 
 def last_race(): #tener ultima carrera
