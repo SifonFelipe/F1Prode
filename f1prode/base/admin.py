fe99388d282df8a22c79ee9f0279b9649a1fd06c
SifonFelipe <supermegafelipe@gmail.com>
2024-06-21 15:25:37 -0300

    models added

--- f1prode/base/admin.py
+++ f1prode/base/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Driver, Group
+from .models import Driver, Group, Prediction
 
 admin.site.register(Driver)
 admin.site.register(Group)
+admin.site.register(Prediction)
