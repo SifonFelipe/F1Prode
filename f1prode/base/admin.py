f49ee4906fdc616bb7047bcfa6743b841255f3af
SifonFelipe <supermegafelipe@gmail.com>
2024-06-24 22:04:49 -0300

    Profile class added, some changes in other classes

--- f1prode/base/admin.py
+++ f1prode/base/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Driver, Group, Prediction, RaceResult, RaceInformation
+from .models import Driver, Group, Prediction, RaceResult, RaceInformation, Profile
 
 admin.site.register(Driver)
 admin.site.register(Group)
 admin.site.register(Prediction)
 admin.site.register(RaceResult)
 admin.site.register(RaceInformation)
+admin.site.register(Profile)
