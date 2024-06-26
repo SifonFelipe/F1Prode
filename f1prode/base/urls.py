0e277694b58cb01730dd84da9b76baf06d39eb3b
SifonFelipe <supermegafelipe@gmail.com>
2024-06-25 23:28:57 -0300

    paths

--- f1prode/base/urls.py
+++ f1prode/base/urls.py
@@ -8,9 +8,12 @@ urlpatterns = [
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     path('logout/', views.logoutView, name='logout-page'),
-    path('creategroup/', views.creategroup, name='creategroup-page'),
-    path('groups/<str:groupname>/', views.viewgroup, name='viewgroup-page'),
     path('predictions/self/', views.view_prediction_result, name='view-prediction-page'),
     path('updatedatabase/race/', views.update_database_race, name='update-database-race'),
-    path('getresults/', views.get_results, name='getresults')
+    path('getresults/', views.get_results, name='getresults'),
+    path('groups/', views.see_groups, name='groups-page'),
+    path('groups/<str:group_id>', views.view_group, name='view-group-page'),
+    path('groups/create/', views.create_groups, name='create-group-page'),
+    path('groups/<str:group_id>/join/', views.request_group_join, name='request-group-page'),
+    path('groups/manage-requests/', views.manage_group_requests, name='manage-requests-page'),
 ]
